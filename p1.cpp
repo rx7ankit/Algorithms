c7bae0f204cf170322bb73608b5783ca1adaca29
rx7ankit <rx7ankit@gmail.com>
2023-08-26 00:06:01 +0530

    p1 file cleared

--- p1.cpp
+++ p1.cpp
@@ -15,37 +15,6 @@ signed main()
     ios_base::sync_with_stdio(false);
     cin.tie(nullptr);
     //=============================================
-    int test;
-    cin >> test;
-    while (test--)
-    {
-        int n;
-        cin >> n;
-        int res;
-        int low = 1, high = min(n, 2648956421);
-        while (low <= high)
-        {
-            int mid = (low + high) / 2;
-            res = mid * (mid - 1) / 2;
-            if (res <= n)
-            {
-                low = mid + 1;
-            }
-            else
-            {
-                high = mid - 1;
-            }
-        }
-        if (res == n)
-        {
-            cout << high << endl;
-        }
-        else
-        {
-            int temp = high * (high - 1) / 2;
-            temp = n - temp;
-            cout << temp + high << endl;
-        }
-    }
+    
     return 0;
 }
