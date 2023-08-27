2d3c2d415d7d28a78ec0a471e74583242f40e5a4
rx7ankit <rx7ankit@gmail.com>
2023-08-27 20:38:03 +0530

    quick sort added

--- sorting.cpp
+++ sorting.cpp
@@ -92,14 +92,47 @@ void recursiveinsertionsort(vector<int> &v, int n, int i)
     }
     recursiveinsertionsort(v, n, i + 1);
 }
-//  snother osrt
+//  ------------------------quick sort -------------------------------------
+int partition(vector<int> &arr, int low, int high)
+{
+    int pivot = arr[low];
+    int i = low;
+    int j = high;
+
+    while (i < j)
+    {
+        while (arr[i] <= pivot && i <= high - 1)
+        {
+            i++;
+        }
+
+        while (arr[j] > pivot && j >= low + 1)
+        {
+            j--;
+        }
+        if (i < j)
+            swap(arr[i], arr[j]);
+    }
+    swap(arr[low], arr[j]);
+    return j;
+}
+
+void qs(vector<int> &arr, int low, int high)
+{
+    if (low < high)
+    {
+        int pIndex = partition(arr, low, high);
+        qs(arr, low, pIndex - 1);
+        qs(arr, pIndex + 1, high);
+    }
+}
 signed main()
 {
     ios_base::sync_with_stdio(false);
     cin.tie(nullptr);
     //=============================================
     vector<int> v = {8, 2, 9, 12, 45, 1, 3, 4, 2, 7};
-    mergeSort(v, 0, v.size()-1);
+    qs(v, 0, v.size() - 1);
     for (auto i : v)
     {
         cout << i << ' ';
